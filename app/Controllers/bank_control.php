23a0f708d003861f9b28a8f5c0d73b8b17bfc8f9
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-10-25 17:57:39 +0530

    conrel

---
+++ app/Controllers/bank_control.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+use CodeIgniter\Controller;
+class bank_control extends Controller{
+    public function index(){
+        return view('bank_view');
+        
+    }
+} 
+?>
