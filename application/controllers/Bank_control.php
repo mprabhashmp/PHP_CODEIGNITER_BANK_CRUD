52e8129e8882befe2cf49cf26e4d8c38c19ab81c
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-11-14 00:02:21 +0530

    update

--- application/controllers/Bank_control.php
+++ application/controllers/Bank_control.php
@@ -1,83 +1,96 @@
 <?php
 
-Class Bank_control extends CI_Controller{ 
-
-    function __construct(){
-
+class Bank_control extends CI_Controller
+{
+    function __construct()
+    {
         parent::__construct();
 
         $this->load->model('Bank_base');
         $this->load->model('Bank_service');
     }
+
+    public function index()
+    {
+       $bank_service = new Bank_service;
+      
     
-    function index(){
-        $bank_service = new Bank_service;
-        $data['item']=$bank_service->getall();
+       $this->db->where('is_deleted IS NULL OR is_deleted = 0', null, false);
+       $query = $this->db->get('bank_details');
+       $data['item'] = $query->result();
 
-        $this->load->view('View_info',$data);
-
+    //$data['item'] = $bank_service->getall();
+       
+       $this->load->view('View_info', $data);
     }
 
-    function create(){
-
+    function create()
+    {
         $this->load->view("Insert_info");
     }
 
-    function saveData(){
-
-        $bank_base = new Bank_base();
-        $bank_service = new Bank_service();
-
-        $bank_base -> setBankName($this->input->post('bank_name'));
-        $bank_base -> setBranch($this->input->post('branch'));
-        $bank_base -> setAccNo($this->input->post('acc_no'));
-
-        $bank_service -> savedetails($bank_base);
-
-        redirect('bank_control/create');
-        echo "<script>alert('Successfully Added !');</script>";
+    function saveData()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('bank_name', 'Bank Name', 'required');
+        $this->form_validation->set_rules('branch', 'Branch', 'required');
+        $this->form_validation->set_rules('acc_no', 'Account Number', 'required');
+    
+        if ($this->form_validation->run() == FALSE) {
+            // Form validation failed, show error messages or redirect back to the form
         
-
-        // print_r($bank_base);
-
+            redirect('bank_control/create');
+        } else {
+            // Validation passed, proceed with saving the data
+            $bank_base = new Bank_base();
+            $bank_service = new Bank_service();
+    
+            $bank_base->setBankName(htmlspecialchars($this->input->post('bank_name'))); // Apply XSS filtering with htmlspecialchars()
+            $bank_base->setBranch(htmlspecialchars($this->input->post('branch'))); // Apply XSS filtering with htmlspecialchars()
+            $bank_base->setAccNo(htmlspecialchars($this->input->post('acc_no'))); // Apply XSS filtering with htmlspecialchars()
+    
+            $bank_service->savedetails($bank_base);
+    
+           
+        }
+  
+        redirect('bank_control/create');
     }
 
-    function edit($id){
 
+    function edit($id)
+    {
         $bank_service = new Bank_service();
         $data['item'] = $bank_service->getedit($id);
-        
-
-        $this->load->view("Update_info",$data);
-
+        $this->load->view("Update_info", $data);
     }
 
-    function updateData(){
-
+    function updateData()
+    {
         $bank_base = new Bank_base();
-        $bank_service = new Bank_service();
+        $bank_service =new Bank_service();
 
-        $bank_base -> setId($this->input->post('id'));
-        $bank_base -> setBankName($this->input->post('bank_name'));
-        $bank_base -> setBranch($this->input->post('branch'));
-        $bank_base -> setAccNo($this->input->post('acc_no'));
+        $bank_base->setId($this->input->post('id'));
+        $bank_base->setBankName($this->input->post('bank_name', true)); // Enable XSS filtering
+        $bank_base->setBranch($this->input->post('branch', true)); // Enable XSS filtering
+        $bank_base->setAccNo($this->input->post('acc_no', true)); // Enable XSS filtering
 
-        $bank_service -> updatedetails($bank_base);
+        $bank_service->updatedetails($bank_base);
 
-        echo "Deleted!";
-        redirect('bank_control/index');
-        
-
-    }
-
-    function delete($id){
-        
-        $bank_service = new Bank_service();
-        $bank_service->deletedetails($id);
-        
-        echo"Deleted";
+        echo "Updated!";
         redirect('bank_control/index');
     }
 
+    public function delete($id, $setFlag = true)
+    {
+            // Set the flag indicating the data was deleted
+            $this->db->set('is_deleted', 1); 
+            $this->db->where('id', $id);
+            $this->db->update('bank_details');
+        
 
+       
+        redirect('bank_control/index');
+
+    }
 }
